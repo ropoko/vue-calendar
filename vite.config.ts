60fe8710ac210e48e9da0b0564a1b1495e042f19
ropoko <rodrigostramantinoli@gmail.com>
2025-11-10 11:24:28 -0300

    feat: added example

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [vue()],
+	build: {
+		lib: {
+			entry: "package/index.ts",
+			name: "VueCalendar",
+			fileName: (format) => `vue-calendar.${format}.js`,
+			formats: ["es", "cjs"],
+		},
+		outDir: "dist",
+		rollupOptions: {
+			external: ["vue"],
+			output: {
+				assetFileNames: (assetInfo) => {
+					if (assetInfo.name === "style.css") return "vue-calendar.css";
+					return assetInfo.name || "asset";
+				},
+			},
+		},
+	},
+});
